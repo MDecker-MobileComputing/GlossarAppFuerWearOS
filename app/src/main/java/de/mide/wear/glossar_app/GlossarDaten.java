c93b7a07dd74336571d5c97b3c820055f79107bf
mide42 <demi_05@arcor.de>
2018-08-19 13:56:58 +0200

    ScrollView in zweite Activity eingebaut; Glossar-Begriffe geaendert.

--- app/src/main/java/de/mide/wear/glossar_app/GlossarDaten.java
+++ app/src/main/java/de/mide/wear/glossar_app/GlossarDaten.java
@@ -2,12 +2,17 @@ package de.mide.wear.glossar_app;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
 
 /**
- * Diese Klasse stellt die anzuzeigenden Glossar-Begriffe zur Verfügung.
+ * Diese Klasse stellt die anzuzeigenden Glossar-Begriffe für den Wissensbereich
+ * "Android-Programmierung" zur Verfügung.
+ * <br><br>
+ *
+ * This project is licensed under the terms of the BSD 3-Clause License.
  */
 public class GlossarDaten {
 
@@ -20,130 +25,74 @@ public class GlossarDaten {
      * (Begriff ist der Key).
      * Begriffe werden in alphabetischer Reihenfolge hinzugefügt, damit man im Quellcode
      * schnell erkennt, ob ein Begriff schon vorhanden ist.
+     * <br><br>
+     *
+     * Diese Methode wird beim ersten Aufruf einer der beiden public-Methoden aufgerufen,
+     * die Glossar initialisiert sich also quasi selbst.
      */
     protected static void initDaten() {
 
         sHashMap = new HashMap<String,String>();
 
-        sHashMap.put("AAPT",
-                "Android Asset Packaging Tool. Programm aus dem Android-SDK, welches u.a. die APK-Datei Installations-Datei) erzeugt.");
+        sHashMap.put("Activity",
+                "Bildschirm-Seite einer Android-App.");
 
         sHashMap.put("ADB",
                 "Android Debug Bridge");
 
-        sHashMap.put("ADT",
-                "Android Development Tools");
-
-        sHashMap.put("AIDL",
-                "Android Interface Description Language");
-
-        sHashMap.put("ANR",
-                "Application not Responsing. Kurzbezeichnung für Fehlermeldung, wenn der Main-Thread einer App zu lange (z.B. länger als 5 Sekunden) blockiert wird.");
-
-        sHashMap.put("AOSP",
-                "Android Open Source Projekt; pflegt Android-Quelltexte.");
+        sHashMap.put("Android Studio",
+                "Offizielle Entwicklungsumgebung für Android-Apps; " +
+                "basiert auf Java-Entwicklungsumgebung \"IntelliJ\" von der Firma JetBrains; " +
+                "vor Android Studio war Eclipse mit dem Plugin \"ADT\" (Android Development Tools) " +
+                " die offizielle IDE für Android.");
 
         sHashMap.put("APK",
-                "Android Package; Dateiformat, in dem eine Android-App auf einem Gerät installiert werden muss.");
+                "Android Package; Dateiformat für die Installationsdatei einer Android-App.");
 
-        sHashMap.put("ART",
-                "Android Runtime; Nachfolger für DVM.");
+        sHashMap.put("ARM",
+                "CPU-Architektur, die in den meisten Android-Geräten (Smartphones & Tablets) sowie " +
+                "Smartwatches und auch iOS-Geräten zum Einsatz kommt.");
 
         sHashMap.put("AVD",
                 "Android Virtual Device; Instanz eines Android-Gerätes im Emulator.");
 
-        sHashMap.put("Bionic",
-                "Unter Android verwendeter Ersatz für die Linux-Standard-C-Bibliothek glib.");
+        sHashMap.put("Extra",
+                "Daten in Form eines Key-Value-Paars, die einem Intent mitgegeben werden können; " +
+                "der Key muss immer ein String sein, für den Value sind verschiedene Datentypen möglich.");
 
-        sHashMap.put("C2DM",
-                "Android Cloud to Device Messaging Service; Push-Dienst seit Android 2.2.");
+        sHashMap.put("IDE",
+                "Integrated Development Environment; Entwicklungsumgebung für Software; " +
+                "für Android-Apps ist derzeit \"Android Studio\" die offizielle IDE.");
 
-        sHashMap.put("Cupcake",
-                "Codename für Android-Version 1.5 (API-Level 3).");
-
-        sHashMap.put("CustomROM",
-                "Inoffizielle Android-Version, Gegenteil von StockROM. Beispiele: CyanogenMod, Replicant, AOKP.");
-
-        sHashMap.put("CyanogenMod",
-                "Beispiel für ein CustomROM, also eine inoffizielle Android-Version.");
-
-        sHashMap.put("Daydream",
-                "Interaktiver Bildschirmschoner");
-
-        sHashMap.put("DDMS",
-                "Dalvik Debug Monitor Service, SDK-Werkzeug und Perspektive in ADT/Eclipse, u.a. für Anzeige Logcat, Zugriff auf Dateisystem und Anzeige Prozesse der Emulator-Instanz.");
-
-        sHashMap.put("DEX",
-                "Dalvik Executable. Binärformat für die Virtual Machine von Android. Alle Class-Dateien einer App befinden sich in EINER DEX-Datei.");
-
-        sHashMap.put("Donut",
-                "Codename für Android-Version 1.6 (API-Level 4).");
-
-        sHashMap.put("DVM",
-                "Dalvik Virtual Machine. Virtuelle Maschine zur Ausführung des Byte-Codes im DEX-Format auf einem Android-Gerät.");
-
-        sHashMap.put("Eclair",
-                "Codename für Android-Version 2.0 bis 2.1.x (API-Level 5 bis 7).");
-
-        sHashMap.put("Froyo",
-                "Frozen Yogurt; Codename für Android-Version 2.2.x (API-Level 8).");
-
-        sHashMap.put("Gingerbread",
-                "Codename für Android-Versionen 2.3 bis 2.3.4 (API-Level 9 & 10).");
-
-        sHashMap.put("Honeycomb",
-                "Codename für Android-Versionen 3.0.x bis 3.2 (API-Level 11 bis 13).");
-
-        sHashMap.put("ICS",
-                "Abkürzung für \"Ice Cream Sandwich\", Codename für Android-Versionen von 4.0 bis 4.0.4 (API-Level 14 & 15)");
+        sHashMap.put("Intent",
+                "Asynchrone Nachricht zwischen verschiedenen Android-Komponenten.");
 
         sHashMap.put("Java ME",
-                "Java MicroEdition, \"Handy-Java\", nicht mit Android kompatibel.");
-
-        sHashMap.put("Jelly Bean",
-                "Codename für Android-Versionen 4.1 bis 4.3.3  (API-Level 16 bis 18).");
-
-        sHashMap.put("KitKat",
-                "Codename für Android-Version 4.3 (API-Level 19).");
-
-        sHashMap.put("Key Lime Pie",
-                "Ursprünglich vermuteter Codename für Android-Version mit API-Level 19.");
-
-        sHashMap.put("MIPS",
-                "CPU-Architektur für embedded Devices, auf die Android auch portiert wurde.");
-
-        sHashMap.put("MR",
-                "Milestone Release; Zusatz für Codename mancher Android-Versionen, z.B. \"Honeycomb_MR2\" für Version 3.2 (API-Level 13).");
-
-        sHashMap.put("NDK",
-                "Native Development Kit, um mit C/C++ Funktionen zu entwickeln, die aus Android heraus aufgerufen werden, z.B. für besonders rechenintensive Operationen.");
-
-        sHashMap.put("Nine-Patch-Grafiken",
-                "Erweiterung für das Grafikformat \"PNG\"; durch einen Rand von einem Pixel Breite kann definiert werden, wie sich die Grafik bei unterschiedlichen Größen verhält, z.B. welche Bereiche gedehnt werden können.");
-
-        sHashMap.put("OHA",
-                "Open Handset Alliance; Organisation von über 80 Technologie-Firmen (z.B. Mobilfunkprovider, Smartphone-Hersteller), die hinter Android steht.");
-
-        sHashMap.put("Rooten",
-                "Android-Gerät z.B. durch Flashen der Firmware so manipulieren, dass dem normalen Endnutzer volle (Admin-)Rechte (Root-Rechte) zur Verfügung stehen.");
-
-        sHashMap.put("SQLite",
-                "Kleines relationales Datenbank-System, das auch in Android zur Verfügung steht.");
-
-        sHashMap.put("StockROM",
-                "Original-ROM von Hersteller, Gegenteil von inoffiziellem CustomROM wie CyanogenMod.");
-
-        sHashMap.put("WebKit",
-                "HTML-Rendering-Engine, wird von Standard-Webbrowser von Android und WebView-Element verwendet.");
-
-        sHashMap.put("Widget",
-                "Kurzform für \"Homescreen Widgets\", auch \"AppWidgets\"; UI auf Homescreen.");
+                "Java MicroEdition; \"Handy-Java\"; nicht kompatibel zu Android.");
     }
 
+    /* *************************************** */
+    /* ********* Start innere Klasse ********* */
+    /* *************************************** */
 
     /**
-     * Liefert sortierte Liste mit allen Glossar-Begriffen
-     * zurück (nur die Begriffe, nicht die Erklärung).
+     * Comparator, der beim Vergleich die Groß-/Kleinschreibung
+     * ignoriert; wird für die Sortierung der Glossar-Begriffe
+     * verwendet.
+     */
+    private static class MeinComparator implements Comparator<String> {
+        @Override
+        public int compare(String str1, String str2) {
+            return str1.compareToIgnoreCase(str2);
+        }
+    };
+    /* *************************************** */
+    /* ********* Ende innere Klasse  ********* */
+    /* *************************************** */
+
+    /**
+     * Liefert sortierte Liste mit allen Glossar-Begriffen zurück (nur die Begriffe, nicht die
+     * Erklärung).
      *
      * @return List mit den Glossar-Begriffe, z.B. "ADB" oder "Daydream".
      */
@@ -152,7 +101,8 @@ public class GlossarDaten {
         if (sHashMap == null) initDaten();
 
         List<String> begriffsListe = new ArrayList<String>( sHashMap.keySet() );
-        Collections.sort(begriffsListe);
+
+        Collections.sort(begriffsListe, new MeinComparator() );
 
         return begriffsListe;
     }
@@ -162,9 +112,13 @@ public class GlossarDaten {
      * Abfragen des Erklärungstextes für einen Glossar-Begriff.
      *
      * @param glossarBegriff Begriff, für den die Erklärung benötigt wird.
+     *
      * @return Erklärung oder <i>null</i>, wenn nicht gefunden.
      */
     public static String getErklaerung(String glossarBegriff) {
+
+        if (sHashMap == null) initDaten();
+
         return sHashMap.get(glossarBegriff);
     }
 
